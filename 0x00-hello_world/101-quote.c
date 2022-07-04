5066d44a6f047339e506916292075d69bcddf79b
Amina <ali.oamina99@gmail.com>
2022-07-04 03:27:18 -0700

    Quote 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <unlstd
+#include <string.h>
+#include <unistd.h>
 
-/*
-* main - prints text
-*
-* Return: Always 0 (Success)
+/**
+ * main -Entry point
+ *
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int length = strlen(text);
+
+	write(2, text, length);
+	return (1);
 }
