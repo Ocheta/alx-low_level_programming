3a6a35a6c18c0dce51d4e4845f0222dcffecf179
aminah <ali.oamina99@gmail.com>
2022-09-11 01:57:04 -0700

    Correcting puts function again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Description: "C program that uses puts function to print a statement"
  *
- *  Return - Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("Programming is like building a multilingual puzzle\n");
 		return (0);
-}	
+}
