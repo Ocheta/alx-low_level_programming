7be40393e233bd23332eb72af23d2786f93d8a22
aminah <ali.oamina99@gmail.com>
2022-09-11 16:54:32 -0700

    Intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,11 +16,11 @@ int main(void)
 	long long int lld;
 	float f;
 
-	printf("Size of a char: %c", sizeof(c));
-	printf("Size of an int: %d", sizeof(i));
-	printf("Size of a long int: %ld", sizeof(ld));
-	printf("Size of a long long int: %lld", sizeof(lld));
-	printf("Size of a float: %f", sizeof(f));
+	printf("Size of a char: %c", sizeof(c)\n);
+	printf("Size of an int: %d", sizeof(i)\n);
+	printf("Size of a long int: %ld", sizeof(ld)\n);
+	printf("Size of a long long int: %lld", sizeof(lld)\n);
+	printf("Size of a float: %f", sizeof(f)\n);
 
 	return (0);
 }
