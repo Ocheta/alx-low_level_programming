07f3e928418a32d5dc5ef3ed70504f4d3dd88534
aminah <ali.oamina99@gmail.com>
2022-09-12 05:18:48 -0700

    Using fwrite

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: "Using fwrite function to print to stderr"
+ *
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	fwrite(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+
+		return (1)
+}
