93d6f38c9a9dd299fde6c821920b6d3d149f80f7
aminah <ali.oamina99@gmail.com>
2022-09-22 21:18:32 -0700

    Comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - for comparing reasons
+ * @s1: parameter 1
+ * @s2: parameter 2
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
