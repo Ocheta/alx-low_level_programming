3d631cb9f3a37e9b20e846cc87ff5c1b8bc59373
aminah <ali.oamina99@gmail.com>
2022-09-23 14:43:00 -0700

    Correcting concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,12 @@ char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
-	while (dest[index++])
+	while (dest[index++] != '\0')
+	{
 	dest_len++;
+	}
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] != '\0'; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
